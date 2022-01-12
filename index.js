d953d965c717e0576186b69367833dc510bd608b
kocmoc-u <coened.beef@gmail.com>
2022-01-12 14:23:25 +0900

    add 4

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 'use strict';
+// httpモジュールを読み込み
 const http = require('http');
-const server = http
-  .createServer((req, res) => {
+// /fsモジュールを読み込み
+// /urlモジュールを読み込み
+
+// createServer()関数で鯖を作成
+const server = http.createServer((req, res) => {
     const now = new Date();
     console.info('[' + now + '] Requested by ' + req.socket.remoteAddress);
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8'
-    });
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
 
     switch (req.method) {
       case 'GET':
@@ -35,13 +37,17 @@ const server = http
         break;
     }
   })
+  // エラーログを出力
   .on('error', e => {
     console.error('[' + new Date() + '] Server Error', e);
   })
   .on('clientError', e => {
     console.error('[' + new Date() + '] Client Error', e);
   });
+
+// ポートを設定
 const port = 8000;
+// 鯖を起動。実行する関数は無名からのデータ取得
 server.listen(port, () => {
   console.info('[' + new Date() + '] Listening on ' + port);
 });
